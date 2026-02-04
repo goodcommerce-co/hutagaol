e48b9c85722f8a018d5763a55bdff5f0b63a8b88
Kevin Chandra <kacol.bot@gmail.com>
2026-02-04 19:09:56 +0700

    feat: use testimonial from json file

--- config.php
+++ config.php
@@ -2,8 +2,22 @@
 
 return [
     'production' => false,
-    'baseUrl' => '',
+    'baseUrl' => 'http://hutagaol.test',
     'siteTitle' => 'Stephan Hutagaol, SH., MKn., MSi., CMed.',
     'description' => 'Official profile of Stephan Hutagaol, an experienced lawyer with over 20 years of practice, international exposure, and global legal connections.',
-    'collections' => [],
+    'collections' => [
+        'testimonials' => [
+            'items' => function ($config) {
+                $testimonials = json_decode(file_get_contents($config['baseUrl'] . '/testimonials.json'));
+                
+                return collect($testimonials)->map(function ($testimony) {
+                    return [
+                        'name' => $testimony->name,
+                        'title'   => $testimony->title,
+                        'testimonial' => $testimony->testimonial,
+                    ];
+                });
+            }
+        ],
+    ],
 ];
